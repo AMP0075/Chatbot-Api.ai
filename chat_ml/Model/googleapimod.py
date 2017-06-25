edcdf91717d584c62a4e777d9a93102ae8f03310
Manobhav Jain <manobhav.jain@gmail.com>
2017-06-25 23:22:47 +0530

    opportunity added

--- chat_ml/Model/googleapimod.py
+++ chat_ml/Model/googleapimod.py
@@ -1,6 +1,9 @@
 import requests
 import json
 import logging
+from chat_ml.models import *
+import numpy
+from sklearn.externals import joblib
 def getResult(q,key):
     # return jobfunc(['data science','python','django','java'],3)
     logger=logging.getLogger(__name__)
@@ -12,14 +15,41 @@ def getResult(q,key):
     print(key)
     j = json.loads(r)
 
-    # if 'intentName' in j['result']['metadata'] and j['result']['metadata']['intentName']=='opportunity':
-    #     l=j['result']['parameters']['SkillSet']
-    #     exp=j['result']['parameters']['experience']
-    #     if j['result']['actionIncomplete']==False:
-    #         return jobfunc(l,exp)
+    if 'intentName' in j['result']['metadata'] and j['result']['metadata']['intentName']=='opportunity' and j['result']['actionIncomplete']==False:
+        l=j['result']['parameters']['SkillSet']
+        exp=j['result']['parameters']['experience']
+        return jobfunc(l,exp)
 
 
 
     return j['result']['fulfillment']['speech']
 
+
+
+def jobfunc(l,exp):
+    res=""
+    myskills=""
+    cv=joblib.load('chat_ml/Data/skillvector.pkl')
+    for i in l:
+        myskills=myskills+i.replace(" ","")+" "
+
+    myskillvector=cv.transform([myskills])
+
+    p = set(Jobs.objects.filter(skillset__name__in=l))
+
+    for i in p:
+        jobskills=""
+        res=res+i.name+"\n"
+
+        for j in i.skillset.all():
+            jobskills=jobskills+ j.name.replace(" ","")+" "
+
+        jobskillvector=cv.transform([jobskills])
+
+
+        commonskill=numpy.logical_and(myskillvector.toarray(),jobskillvector.toarray())
+        res=res+ " for the skill/s-"+" ".join(cv.inverse_transform(commonskill)[0])+" "
+        #print(type(i.skillset.))
+
+    return res
 
