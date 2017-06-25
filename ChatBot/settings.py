2aea4f76f39c99903c2edfde6936d293878e5972
Manobhav Jain <manobhav.jain@gmail.com>
2017-06-25 17:20:45 +0530

    database changed

--- ChatBot/settings.py
+++ ChatBot/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
