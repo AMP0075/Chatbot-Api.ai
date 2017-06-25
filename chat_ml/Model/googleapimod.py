15940672620a28c6aabcb6eaeccf2dcc0c46558f
Manobhav Jain <manobhav.jain@gmail.com>
2017-06-25 22:18:29 +0530

    key added

--- chat_ml/Model/googleapimod.py
+++ chat_ml/Model/googleapimod.py
@@ -1,15 +1,15 @@
 import requests
 import json
 import logging
-def getResult(q,request):
+def getResult(q,key):
     # return jobfunc(['data science','python','django','java'],3)
     logger=logging.getLogger(__name__)
     url = 'https://api.api.ai/v1/query'
-    payload = {'v': '20170101', 'query':q, 'lang': 'en', 'sessionId': request.session.session_key}
+    payload = {'v': '20170101', 'query':q, 'lang': 'en', 'sessionId': key}
     headers = {'Authorization': 'Bearer d3c00533fe4a43b584828eabdc146726'}
     r = requests.get(url, params=payload, headers=headers).text
 
-    logger.info(request.session.session_key)
+    print(key)
     j = json.loads(r)
 
     # if 'intentName' in j['result']['metadata'] and j['result']['metadata']['intentName']=='opportunity':
--- chat_ml/views.py
+++ chat_ml/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 from django.shortcuts import render,HttpResponse,render_to_response
 from django.shortcuts import render, redirect
 from .Model.googleapimod import *
-
+from random import choice
+from string import ascii_uppercase
 
 # Create your views here.
 
 def googleapi(request):
+    if 'key' not in request.session:
+        request.session['key']=''.join(choice(ascii_uppercase) for i in range(10))
     if request.is_ajax():
         q=request.POST['query']
-        ans=getResult(q,request)
+        ans=getResult(q,request.session['key'])
         return HttpResponse(ans)
 
     return render(request,'chat_ml/chatengine.html',{'name':'manobhav'})
